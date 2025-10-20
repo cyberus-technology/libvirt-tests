c866bcf3ab177faa0db6dfac3a2c28c631a96cf7
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2025-10-20 11:05:45 +0200

    fmt: nix files

--- images/nixos-image.nix
+++ images/nixos-image.nix
@@ -53,8 +53,8 @@ nixpkgs.lib.nixosSystem {
           openFirewall = true;
         };
         environment.systemPackages = with pkgs; [
-            screen
-            stress
+          screen
+          stress
         ];
         # pw: root
         users.users.root.initialHashedPassword = lib.mkForce "$y$j9T$HiT/m702z/73g4Dt5RzbW0$b3SaYI1FoyT/ORV/qFR/s9zonJBKDn4p2XKyYM2wp1.";
