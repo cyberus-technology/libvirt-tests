5c0a6845238ddefaf74c87da29134ccebdafeee0
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2025-11-26 09:24:08 +0100

    nixos-image: restructure options
    - group options
    - sort groups alphabetically
    
    On-behalf-of: SAP philipp.schuster@sap.com

--- images/nixos-image.nix
+++ images/nixos-image.nix
@@ -1,4 +1,4 @@
-# Builds a small NixOS-based bootable image.
+# Builds a small NixOS-based bootable image (iso).
 
 {
   nixpkgs,
@@ -25,68 +25,23 @@ nixpkgs.lib.nixosSystem {
           # of an iso, but works nonetheless.
           "${modulesPath}/installer/cd-dvd/installation-cd-minimal-new-kernel-no-zfs.nix"
         ];
-        system.stateVersion = "25.05";
 
-        nix.enable = false;
-
-        boot.kernelParams = [
-          "console=ttyS0"
-          "earlyprintk=ttyS0"
+        boot.initrd.availableKernelModules = [
+          "virtio_blk"
+          "virtio_pci"
         ];
-
+        boot.initrd.kernelModules = [ "virtio_net" ];
         # 6.17 has a broken virtio-net driver. As this image runs in a CHV VM
         # with a virtio-net device for communication with the outer world, we
         # stick to a LTS kernel for now.
         # https://github.com/cloud-hypervisor/cloud-hypervisor/issues/7447
         boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_12;
-
-        hardware.enableAllHardware = lib.mkForce false;
-        hardware.enableRedistributableFirmware = false;
-
-        isoImage.makeUsbBootable = true;
-        isoImage.makeEfiBootable = true;
-        isoImage.makeBiosBootable = false;
-
-        boot.initrd.availableKernelModules = [
-          "virtio_pci"
-          "virtio_blk"
+        boot.kernelParams = [
+          "console=ttyS0"
+          "earlyprintk=ttyS0"
         ];
-        boot.initrd.kernelModules = [ "virtio_net" ];
-
         boot.loader.timeout = lib.mkForce 0;
-        networking.hostName = "nixos";
-        networking.firewall.enable = false;
-        networking.useDHCP = false;
-        networking.useNetworkd = true;
-        networking.interfaces.eth1337.useDHCP = false;
-        networking.interfaces.eth1337.ipv4.addresses = [
-          {
-            address = "192.168.1.2";
-            prefixLength = 24;
-          }
-        ];
 
-        systemd.network.wait-online.ignoredInterfaces = [
-          "eth1337"
-        ];
-
-        services.openssh = {
-          enable = true;
-          settings = {
-            PermitRootLogin = "yes";
-            PasswordAuthentication = true;
-          };
-          openFirewall = true;
-          hostKeys = [
-            {
-              path = "/etc/ssh/ssh_host_ed25519_key";
-              type = "ed25519";
-            }
-          ];
-        };
-
-        # We use a dummy key for the test VM to shortcut the boot time.
-        systemd.services.sshd-keygen.enable = false;
         environment.etc = {
           "ssh/ssh_host_ed25519_key" = {
             mode = "0600";
@@ -105,18 +60,60 @@ nixpkgs.lib.nixosSystem {
             source = pkgs.writers.writeText "ssh_host_ed25519_key.pub" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKXYPRt5N8EZQT4jIS+N+zxs6qhOnCbU/NhbC9QJsU/4 test@testvm";
           };
         };
-
         environment.systemPackages = with pkgs; [
           screen
           stress
         ];
 
+        isoImage.makeUsbBootable = true;
+        isoImage.makeEfiBootable = true;
+        isoImage.makeBiosBootable = false;
+
+        hardware.enableAllHardware = lib.mkForce false;
+        hardware.enableRedistributableFirmware = false;
+
+        networking.firewall.enable = false;
+        networking.hostName = "nixos";
+        networking.interfaces.eth1337.ipv4.addresses = [
+          {
+            address = "192.168.1.2";
+            prefixLength = 24;
+          }
+        ];
+        networking.interfaces.eth1337.useDHCP = false;
+        networking.useDHCP = false;
+        networking.useNetworkd = true;
+
+        nix.enable = false;
+
+        services.openssh = {
+          enable = true;
+          settings = {
+            PermitRootLogin = "yes";
+            PasswordAuthentication = true;
+          };
+          openFirewall = true;
+          hostKeys = [
+            {
+              path = "/etc/ssh/ssh_host_ed25519_key";
+              type = "ed25519";
+            }
+          ];
+        };
         services.udev.extraRules = ''
           # Stable NIC name for known test VM MAC
           ACTION=="add", SUBSYSTEM=="net", \
             ATTR{address}=="${mac}", \
             NAME="eth1337"
         '';
+
+        system.stateVersion = "25.05";
+
+        systemd.network.wait-online.ignoredInterfaces = [
+          "eth1337"
+        ];
+        # We use a dummy key for the test VM to shortcut the boot time.
+        systemd.services.sshd-keygen.enable = false;
 
         # pw: root
         users.users.root.initialHashedPassword = lib.mkForce "$y$j9T$HiT/m702z/73g4Dt5RzbW0$b3SaYI1FoyT/ORV/qFR/s9zonJBKDn4p2XKyYM2wp1.";
