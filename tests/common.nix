ab960645bc39a1f46b4f4a47d2f143ade359a30f
Pascal Scholz <pascal.scholz@cyberus-technology.de>
2025-12-04 14:28:30 +0100

    nix: Disable DHCP
    We don't use the DHCP anymore, so we can deactivate it.
    
    Signed-off-by: Pascal Scholz <pascal.scholz@cyberus-technology.de>
    On-behalf-of: SAP pascal.scholz@sap.com

--- tests/common.nix
+++ tests/common.nix
@@ -263,7 +263,7 @@ in
         matchConfig.Name = "br0";
         networkConfig = {
           Description = "Main Bridge";
-          DHCPServer = "yes";
+          DHCPServer = "no";
         };
 
         # Static IP configuration for the bridge itself
