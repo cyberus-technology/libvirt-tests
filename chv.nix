20201cc92bfe90fe310e8def0708dcdc3dc9c8e9
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2025-12-17 11:42:49 +0100

    nix/chv: adapt to new crate structure
    Since PR #7525 (Dec 2025)[0], Cloud Hypervisor uses a virtual Cargo manifest and
    the main package was moved to the `./cloud-hypervisor` subdirectory. This change
    is effective since v50. As our v49-based CHV version backported these changes
    already, we need to adapt the Nix build of CHV.
    
    [0] https://github.com/cloud-hypervisor/cloud-hypervisor/pull/7525
    
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>
    On-behalf-of: SAP philipp.schuster@sap.com

--- chv.nix
+++ chv.nix
@@ -14,38 +14,42 @@ let
   # Crane lib with proper Rust toolchain
   craneLib' = craneLib.overrideToolchain rustToolchain;
 
-  commonArgs = {
-    meta = cloud-hypervisor-meta;
+  commonArgs =
+    let
+      src = craneLib'.cleanCargoSource cloud-hypervisor-src;
+    in
+    {
+      inherit src;
+      # Since Nov 2025 (v50), Cloud Hypervisor has a virtual manifest and the
+      # main package was moved into a sub directory.
+      cargoToml = "${src}/cloud-hypervisor/Cargo.toml";
+      meta = cloud-hypervisor-meta;
 
-    src = craneLib'.cleanCargoSource cloud-hypervisor-src;
+      # Pragmatic release profile with debug-ability and faster
+      # compilation times in mind.
+      env = {
+        CARGO_PROFILE_RELEASE_DEBUG_ASSERTIONS = "true";
+        CARGO_PROFILE_RELEASE_OPT_LEVEL = 2;
+        CARGO_PROFILE_RELEASE_OVERFLOW_CHECKS = "true";
+        CARGO_PROFILE_RELEASE_LTO = "no";
+      };
 
-    # Pragmatic release profile with debug-ability and faster
-    # compilation times in mind.
-    env = {
-      CARGO_PROFILE_RELEASE_DEBUG_ASSERTIONS = "true";
-      CARGO_PROFILE_RELEASE_OPT_LEVEL = 2;
-      CARGO_PROFILE_RELEASE_OVERFLOW_CHECKS = "true";
-      CARGO_PROFILE_RELEASE_LTO = "no";
+      nativeBuildInputs = [
+        pkg-config
+      ];
+      buildInputs = [
+        openssl
+      ];
+      # Fix build. Reference:
+      # - https://github.com/sfackler/rust-openssl/issues/1430
+      # - https://docs.rs/openssl/latest/openssl/
+      OPENSSL_NO_VENDOR = true;
     };
-
-    nativeBuildInputs = [
-      pkg-config
-    ];
-    buildInputs = [
-      openssl
-    ];
-    # Fix build. Reference:
-    # - https://github.com/sfackler/rust-openssl/issues/1430
-    # - https://docs.rs/openssl/latest/openssl/
-    OPENSSL_NO_VENDOR = true;
-  };
 
   # Downloaded and compiled dependencies.
   cargoArtifacts = craneLib'.buildDepsOnly (
     commonArgs
     // {
-      # "suffix '-deps' will be appended
-      pname = "cloud-hypervisor";
       doCheck = false;
     }
   );
@@ -54,7 +58,6 @@ let
     commonArgs
     // {
       inherit cargoArtifacts;
-      pname = "cloud-hypervisor";
       # Don't execute tests here. We want this in a dedicated step.
       doCheck = false;
       cargoExtraArgs = "--features kvm";
