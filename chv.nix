c76ccd346008889d573d66166891ba3e07c83596
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-01-07 07:36:09 +0100

    flake/chv: bump cloud-hypervisor-src + adapt to new crate structure
    Since PR #7525 (Dec 2025)[0], Cloud Hypervisor uses a virtual Cargo manifest and
    the main package was moved to the `./cloud-hypervisor` subdirectory. This change
    is effective since v50, which we just upgraded our Cloud Hypervisor patchset
    to [1].
    
    [0] https://github.com/cloud-hypervisor/cloud-hypervisor/pull/7525
    [1] https://github.com/cyberus-technology/cloud-hypervisor/pull/60
    
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>
    On-behalf-of: SAP philipp.schuster@sap.com

--- chv.nix
+++ chv.nix
@@ -14,31 +14,39 @@ let
   # Crane lib with proper Rust toolchain
   craneLib' = craneLib.overrideToolchain rustToolchain;
 
-  commonArgs = {
-    meta = cloud-hypervisor-meta;
-    src = craneLib'.cleanCargoSource cloud-hypervisor-src;
+  commonArgs =
+    let
+      src = craneLib'.cleanCargoSource cloud-hypervisor-src;
+    in
+    {
+      inherit src;
+      # Since Nov 2025 (v50), Cloud Hypervisor has a virtual manifest and the
+      # main package was moved into a sub directory.
+      cargoToml = "${src}/cloud-hypervisor/Cargo.toml";
 
-    # Pragmatic release profile with debug-ability and faster
-    # compilation times in mind.
-    env = {
-      CARGO_PROFILE_RELEASE_DEBUG_ASSERTIONS = "true";
-      CARGO_PROFILE_RELEASE_OPT_LEVEL = 2;
-      CARGO_PROFILE_RELEASE_OVERFLOW_CHECKS = "true";
-      CARGO_PROFILE_RELEASE_LTO = "no";
+      meta = cloud-hypervisor-meta;
 
-      # Fix build. Reference:
-      # - https://github.com/sfackler/rust-openssl/issues/1430
-      # - https://docs.rs/openssl/latest/openssl/
-      OPENSSL_NO_VENDOR = true;
+      # Pragmatic release profile with debug-ability and faster
+      # compilation times in mind.
+      env = {
+        CARGO_PROFILE_RELEASE_DEBUG_ASSERTIONS = "true";
+        CARGO_PROFILE_RELEASE_OPT_LEVEL = 2;
+        CARGO_PROFILE_RELEASE_OVERFLOW_CHECKS = "true";
+        CARGO_PROFILE_RELEASE_LTO = "no";
+
+        # Fix build. Reference:
+        # - https://github.com/sfackler/rust-openssl/issues/1430
+        # - https://docs.rs/openssl/latest/openssl/
+        OPENSSL_NO_VENDOR = true;
+      };
+
+      nativeBuildInputs = [
+        pkg-config
+      ];
+      buildInputs = [
+        openssl
+      ];
     };
-
-    nativeBuildInputs = [
-      pkg-config
-    ];
-    buildInputs = [
-      openssl
-    ];
-  };
 
   # Downloaded and compiled dependencies.
   cargoArtifacts = craneLib'.buildDepsOnly (
