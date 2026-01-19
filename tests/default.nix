ce549269e3ca870969b6ccde5fa8eeafbb12a092
Stefan Kober <stefan.kober@cyberus-technology.de>
2026-01-19 16:08:01 +0100

    nix: add nixos test variants without port forward
    This allows to start the nixos test suite without port forwarding via a
    convinient passthru attribute.
    
    Doing so makes it easier to start multiple libvirt-test suites on the
    same host in parallel. Note that the directory for the VM disk images
    might needs to be randomized to prevent clashes there.
    
    On-behalf-of: SAP stefan.kober@sap.com

--- tests/default.nix
+++ tests/default.nix
@@ -3,54 +3,71 @@
   libvirt-src,
   nixos-image,
   chv-ovmf,
+  enablePortForwarding ? true,
 }:
+let
+  # Function to add a passthru attribute to a nixos test derivation that
+  # disables port forwarding. The non port forwarding version will mainly be
+  # used in the CI.
+  addNoPortForwardingAttr =
+    _: drv:
+    drv
+    // {
+      passthru.no_port_forwarding = drv.override { enablePortForwarding = false; };
+    };
 
-rec {
-  default = pkgs.callPackage ./libvirt-test.nix {
-    inherit
-      libvirt-src
-      nixos-image
-      chv-ovmf
-      ;
-    testScriptFile = ./testsuite_default.py;
-  };
+  tests = {
+    default = pkgs.callPackage ./libvirt-test.nix {
+      inherit
+        libvirt-src
+        nixos-image
+        chv-ovmf
+        enablePortForwarding
+        ;
+      testScriptFile = ./testsuite_default.py;
+    };
 
-  live_migration = pkgs.callPackage ./libvirt-test.nix {
-    inherit
-      libvirt-src
-      nixos-image
-      chv-ovmf
-      ;
-    testScriptFile = ./testsuite_migration.py;
-  };
+    live_migration = pkgs.callPackage ./libvirt-test.nix {
+      inherit
+        libvirt-src
+        nixos-image
+        chv-ovmf
+        enablePortForwarding
+        ;
+      testScriptFile = ./testsuite_migration.py;
+    };
 
-  hugepage = pkgs.callPackage ./libvirt-test.nix {
-    inherit
-      libvirt-src
-      nixos-image
-      chv-ovmf
-      ;
-    testScriptFile = ./testsuite_hugepages.py;
-  };
+    hugepage = pkgs.callPackage ./libvirt-test.nix {
+      inherit
+        libvirt-src
+        nixos-image
+        chv-ovmf
+        enablePortForwarding
+        ;
+      testScriptFile = ./testsuite_hugepages.py;
+    };
 
-  long_migration_with_load = pkgs.callPackage ./libvirt-test.nix {
-    inherit
-      libvirt-src
-      nixos-image
-      chv-ovmf
-      ;
-    testScriptFile = ./testsuite_long_migration_with_load.py;
+    long_migration_with_load = pkgs.callPackage ./libvirt-test.nix {
+      inherit
+        libvirt-src
+        nixos-image
+        chv-ovmf
+        enablePortForwarding
+        ;
+      testScriptFile = ./testsuite_long_migration_with_load.py;
+    };
   };
 
   # Convenience attribute containing all nixos test driver attributes mainly
   # used for evaluation checks
   all = pkgs.symlinkJoin {
     name = "all-test-driver";
-    paths = [
+    paths = with tests; [
       default.driver
       live_migration.driver
       hugepage.driver
       long_migration_with_load.driver
     ];
   };
-}
+in
+(builtins.mapAttrs addNoPortForwardingAttr tests) // { inherit all; }
