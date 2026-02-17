879b2b6caf09ef84c235e53c2d8b3e820a81bace
Pascal Scholz <pascal.scholz@cyberus-technology.de>
2026-02-17 15:20:46 +0100

    tests: Refactor `default.nix`
    We had multiple calls to `libvirt-test.nix` with the same arguments for
    every test suite. A helper function can abstract this way, so only the
    invocation of the helper with the right arguments remains. With the
    helper implemented, we can reduce code duplication and achieve higher
    maintainability.
    
    Signed-off-by: Pascal Scholz <pascal.scholz@cyberus-technology.de>
    On-behalf-of: SAP pascal.scholz@sap.com

--- tests/default.nix
+++ tests/default.nix
@@ -6,6 +6,22 @@
   enablePortForwarding ? true,
 }:
 let
+  createTestSuite =
+    {
+      testScriptFile,
+      enablePortForwarding,
+      numaHosts ? false,
+    }:
+    pkgs.callPackage ./libvirt-test.nix {
+      inherit
+        libvirt
+        nixos-image
+        chv-ovmf
+        testScriptFile
+        enablePortForwarding
+        numaHosts
+        ;
+    };
   # Function to add a passthru attribute to a nixos test derivation that
   # disables port forwarding. The non port forwarding version will mainly be
   # used in the CI.
@@ -17,53 +33,28 @@ let
     };
 
   tests = {
-    default = pkgs.callPackage ./libvirt-test.nix {
-      inherit
-        libvirt
-        nixos-image
-        chv-ovmf
-        enablePortForwarding
-        ;
+    default = createTestSuite {
+      inherit enablePortForwarding;
       testScriptFile = ./testsuite_default.py;
     };
 
-    live_migration = pkgs.callPackage ./libvirt-test.nix {
-      inherit
-        libvirt
-        nixos-image
-        chv-ovmf
-        enablePortForwarding
-        ;
-      testScriptFile = ./testsuite_migration.py;
+    live_migration = createTestSuite {
+      inherit enablePortForwarding;
+      testScriptFile = ././testsuite_migration.py;
     };
 
-    hugepage = pkgs.callPackage ./libvirt-test.nix {
-      inherit
-        libvirt
-        nixos-image
-        chv-ovmf
-        enablePortForwarding
-        ;
+    hugepage = createTestSuite {
+      inherit enablePortForwarding;
       testScriptFile = ./testsuite_hugepages.py;
     };
 
-    long_migration_with_load = pkgs.callPackage ./libvirt-test.nix {
-      inherit
-        libvirt
-        nixos-image
-        chv-ovmf
-        enablePortForwarding
-        ;
+    long_migration_with_load = createTestSuite {
+      inherit enablePortForwarding;
       testScriptFile = ./testsuite_long_migration_with_load.py;
     };
 
-    numa_hosts = pkgs.callPackage ./libvirt-test.nix {
-      inherit
-        libvirt
-        nixos-image
-        chv-ovmf
-        enablePortForwarding
-        ;
+    numa_hosts = createTestSuite {
+      inherit enablePortForwarding;
       testScriptFile = ./testsuite_numa.py;
       numaHosts = true;
     };
@@ -73,12 +64,9 @@ let
   # used for evaluation checks
   all = pkgs.symlinkJoin {
     name = "all-test-driver";
-    paths = with tests; [
-      default.driver
-      live_migration.driver
-      hugepage.driver
-      long_migration_with_load.driver
-      numa_hosts.driver
+    paths = pkgs.lib.pipe tests [
+      builtins.attrValues
+      (map (t: t.driver))
     ];
   };
 in
