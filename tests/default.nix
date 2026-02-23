6f3294cd36a9873e20bf42d82017951bd7dcba6f
Stefan Kober <stefan.kober@cyberus-technology.de>
2026-02-23 10:40:10 +0100

    tests: refactor NUMA test
    Instead of passing some boolean that configures if the current test
    should have the NUMA test specific adjustments, we allow passing extra
    configuration to the Controller and Compute VM. This way, we can inject
    the NUMA specific configurations in an elegant way.
    
    Further, this allows other tests to make specific adjustments to the
    compute and controller VM too.
    
    On-behalf-of: SAP stefan.kober@sap.com

--- tests/default.nix
+++ tests/default.nix
@@ -6,11 +6,16 @@
   enablePortForwarding ? true,
 }:
 let
+  numaConf = import ./numa-domain-xml.nix {
+    inherit nixos-image;
+    inherit (pkgs) writeText;
+  };
   createTestSuite =
     {
       testScriptFile,
       enablePortForwarding,
-      numaHosts ? false,
+      extraControllerConfig ? [ ],
+      extraComputeConfig ? [ ],
     }:
     pkgs.callPackage ./libvirt-test.nix {
       inherit
@@ -19,7 +24,8 @@ let
         chv-ovmf
         testScriptFile
         enablePortForwarding
-        numaHosts
+        extraControllerConfig
+        extraComputeConfig
         ;
     };
   # Function to add a passthru attribute to a nixos test derivation that
@@ -56,7 +62,44 @@ let
     numa_hosts = createTestSuite {
       inherit enablePortForwarding;
       testScriptFile = ./testsuite_numa.py;
-      numaHosts = true;
+      extraComputeConfig = [
+        numaConf
+        (
+          { ... }:
+          {
+            virtualisation.qemu.options =
+
+              [
+                "-smp 4,sockets=2,cores=2,threads=1"
+                "-object memory-backend-ram,size=2G,id=m0"
+                "-object memory-backend-ram,size=2G,id=m1"
+                "-numa node,nodeid=0,cpus=0-1,memdev=m0"
+                "-numa node,nodeid=1,cpus=2-3,memdev=m1"
+              ];
+          }
+        )
+      ];
+      extraControllerConfig = [
+        numaConf
+        (
+          { ... }:
+          {
+            virtualisation.qemu.options =
+
+              [
+                "-smp 4,sockets=4,cores=1,threads=1"
+                "-object memory-backend-ram,size=1G,id=m0"
+                "-object memory-backend-ram,size=1G,id=m1"
+                "-object memory-backend-ram,size=1G,id=m2"
+                "-object memory-backend-ram,size=1G,id=m3"
+                "-numa node,nodeid=0,cpus=0,memdev=m0"
+                "-numa node,nodeid=1,cpus=1,memdev=m1"
+                "-numa node,nodeid=2,cpus=2,memdev=m2"
+                "-numa node,nodeid=3,cpus=3,memdev=m3"
+              ];
+          }
+        )
+      ];
     };
   };
 
